e972d300f7a22fcb372da80c4e34f8eafece6ae8
Olena_Krasylshchykova <olena_krasylshchykova@epam.com>
2023-03-10 18:37:56 +0100

    tasks 18-19 commit

--- jmp-dto/src/dto/User.java
+++ jmp-dto/src/dto/User.java
@@ -10,6 +10,30 @@ public class User {
     String surname;
     LocalDate birthday;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
     @Override
     public String toString() {
         return "User{" +
--- jmp-service-api/src/service/api/Service.java
+++ jmp-service-api/src/service/api/Service.java
@@ -4,6 +4,8 @@ import dto.BankCard;
 import dto.Subscription;
 import dto.User;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +13,15 @@ public interface Service {
 
     void subscribe(BankCard bankCard);
     Optional<Subscription> getSubscriptionByBankCardNumber(String bankCardNumber);
-
     List<User> getAllUsers();
+    int PAYABLE_AGE   = 18;
+    default double getAverageUsersAge(){
+        return getAllUsers().stream().
+                mapToLong(user -> ChronoUnit.YEARS.between(user.getBirthday(), LocalDate.now())).average().getAsDouble();
+    }
+    static boolean isPayableUser(User user){
+            return ChronoUnit.YEARS.between(user.getBirthday(),
+                    LocalDate.now())>PAYABLE_AGE;
+    }
+
 }
