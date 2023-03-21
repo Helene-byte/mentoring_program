9772aa811bedd60dd5f51242a4fc59c2041cc044
Olena_Krasylshchykova <olena_krasylshchykova@epam.com>
2023-03-21 16:28:13 +0100

    removing comments

--- jmp-cache-service/src/cache/service/MemoryCacheTest.java
+++ jmp-cache-service/src/cache/service/MemoryCacheTest.java
@@ -15,9 +15,6 @@ public class MemoryCacheTest {
             cache.testObjectsCleanupTime();
         }
         private void testAddRemoveObjects() {
-            // Test with crunchifyTimeToLive = 200 seconds
-            // crunchifyTimerInterval = 500 seconds
-            // maxItems = 6
            MemoryInCache<String, String> cache = new MemoryInCache<String, String>(200, 500, 6);
             cache.put("eBay", "eBay");
             cache.put("Paypal", "Paypal");
@@ -40,30 +37,16 @@ public class MemoryCacheTest {
         }
 
         private void testExpiredCacheObjects() throws InterruptedException {
-            // Test with crunchifyTimeToLive = 1 second
-            // crunchifyTimerInterval = 1 second
-            // maxItems = 10
             MemoryInCache<String, String> cache = new MemoryInCache<String, String>(1, 1, 10);
             cache.put("eBay", "eBay");
             cache.put("Paypal", "Paypal");
-
-            // Adding 3 seconds sleep. Both above objects will be removed from
-            // Cache because of timeToLiveInSeconds value
             Thread.sleep(3000);
-
             printActivity("Two objects are added but reached timeToLive. cache.size(): " + cache.size());
         }
         private void testObjectsCleanupTime() throws InterruptedException {
             int size = 500000;
-            // Test with timeToLiveInSeconds = 100 seconds
-            // timerIntervalInSeconds = 100 seconds
-            // maxItems = 500000
             MemoryInCache<String, String> cache = new MemoryInCache<String, String>(100, 100, 500000);
             for (int i = 0; i < size; i++) {
-
-                // toString(): Returns a String object representing the specified integer.
-                // The argument is converted to signed decimal representation and returned as a string,
-                // exactly as if the argument and radix 10 were given as arguments to the toString(int, int) method.
                 String value = Integer.toString(i);
                 cache.put(value, value);
             }
